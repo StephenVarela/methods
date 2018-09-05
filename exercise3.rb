33130504d19f0e2f81cc648ca6f0eb9341f9e704
StephenVarela <varelast92@gmail.com>
2018-09-05 15:18:44 -0400

    added is even method to exercise 3

---
+++ exercise3.rb
@@ -0,0 +1,13 @@
+def is_even(my_number)
+  if(my_number%2 == 1)
+    return false
+  else
+    return true
+  end
+end
+
+puts is_even(2)
+puts is_even(3)
+puts is_even(15)
+puts is_even(10)
+puts is_even(20)
