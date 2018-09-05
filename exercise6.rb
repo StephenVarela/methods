353c8cb2062ee7430337f8ae04f6a3ace9fe76bc
StephenVarela <varelast92@gmail.com>
2018-09-05 16:49:03 -0400

    limit repeating decimal to 2

--- exercise6.rb
+++ exercise6.rb
@@ -1,7 +1,7 @@
 def F_to_C(temp_F)
   fahrenheit = temp_F.to_i
   celsius = (fahrenheit - 32.0) * 5.0/9.0
-  return celsius
+  return celsius.round(2)
 end
 
 puts "What is the temperature in fahrenheit"
