a4bebeef30681a539e3aec3e188a566407d04202
StephenVarela <varelast92@gmail.com>
2018-09-05 15:15:49 -0400

    finished exercise 2 added is negative method

---
+++ exercise2.rb
@@ -0,0 +1,12 @@
+def is_negative(my_number)
+  if(my_number < 0 )
+    return true
+  else(my_number < 0 )
+    return false
+  end
+end
+
+puts is_negative(-5)
+puts is_negative(1)
+puts is_negative(-7651)
+puts is_negative(55)
